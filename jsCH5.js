8b71432b824c63dd15ad1be0ed978145d109a482
andiniseptia <andiniseptiandri124@gmail.com>
2024-04-02 13:55:28 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+document.getElementById("video").addEventListener("click", function() {
+    alert("Memutar Video Keindahan Alam Indonesia");
+});
+
+document.getElementById("audio").addEventListener("click", function() {
+    alert("Memutar Audio Suasana Alam");
+});
+
+document.getElementById("gambar").addEventListener("click", function() {
+    alert("Menampilkan Gambar Keindahan Alam Indonesia");
+});
